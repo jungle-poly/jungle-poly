e1cae4543dee5492e406e5b3900bed07c2ceaa45
PeterChoi <soww12345@gmail.com>
2024-03-20 23:54:52 +0900

    update: 기본포트 변경

--- app.py
+++ app.py
@@ -240,5 +240,5 @@ def invalid_token_callback(error_string):
 
 
 if __name__ == '__main__':
-    # app.run('0.0.0.0', port=5000, debug=True)
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
+    # app.run('0.0.0.0', port=5001, debug=True)
